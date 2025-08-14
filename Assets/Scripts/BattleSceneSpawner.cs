116a90e0d83f18dd5a8d137456a4f487be171d83
unknown <kienndpk04097@gmail.com>
2025-08-14 15:07:38 +0700

    hoan thanh pvp  di chuyen

--- Assets/Scripts/BattleSceneSpawner.cs
+++ Assets/Scripts/BattleSceneSpawner.cs
@@ -13,14 +13,35 @@ public class BattleSceneSpawner : MonoBehaviour
         int p2Index = PlayerPrefs.GetInt("Player2Character", 0);
 
         var keyboard = InputSystem.GetDevice<Keyboard>();
-        var gamepad = InputSystem.GetDevice<Gamepad>();
 
-        var player1 = PlayerInput.Instantiate(characterPrefabs[p1Index], controlScheme: "Keyboard&Mouse", pairWithDevice: keyboard);
+        // Spawn Player 1 - Keyboard1
+        var player1 = PlayerInput.Instantiate(
+            characterPrefabs[p1Index],
+            controlScheme: "Keyboard1",
+            pairWithDevice: keyboard
+        );
         player1.transform.position = player1Spawn.position;
         player1.transform.rotation = Quaternion.identity;
 
-        var player2 = PlayerInput.Instantiate(characterPrefabs[p2Index], controlScheme: "Gamepad", pairWithDevice: gamepad);
+        // Spawn Player 2 - Keyboard2
+        var player2 = PlayerInput.Instantiate(
+            characterPrefabs[p2Index],
+            controlScheme: "Keyboard2",
+            pairWithDevice: keyboard
+        );
         player2.transform.position = player2Spawn.position;
         player2.transform.rotation = Quaternion.identity;
+
+        // Gán layer Player2
+        SetLayerRecursively(player2.gameObject, LayerMask.NameToLayer("Player2"));
+    }
+
+    void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
     }
 }
